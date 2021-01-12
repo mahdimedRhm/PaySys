91551f0fd6622a3483db431c161bf53c5770ccc9
RAHMOUN Mohamed Elmehdi <mahdimedrhm@gmail.com>
2021-01-12 22:10:23 +0100

    feat: install JSEncrypt

--- spa/src/main.js
+++ spa/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import { JSEncrypt } from 'jsencrypt';
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
@@ -11,3 +12,13 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.prototype.$encryptedData = function(publicKey, data) {
+  //new an object
+  let encrypt = new JSEncrypt()
+  //Setting public key
+  encrypt.setPublicKey(publicKey)
+  //password is the data to be encrypted. You don't need to pay attention to the + sign here, because rsa itself has already transcoded base64, and there is no +. It's all binary data
+  let result = encrypt.encrypt(password);
+  return result
+}
