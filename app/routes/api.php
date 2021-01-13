f286b8e905281db9b7c91c850b1c5235ae89845f
RAHMOUN Mohamed Elmehdi <mahdimedrhm@gmail.com>
2021-01-13 22:49:06 +0100

    refact: card and auth routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -17,23 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => 'api', 'namespace' => 'App\Http\Controllers' ], function ($router){
+    Route::group(['prefix' => 'auth'], function ($router) {
+        Route::post('register', 'AuthController@register');
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+        Route::get('test', 'AuthController@test');
+        Route::post('transaction', 'AuthController@addTransaction');
+        Route::get('pubkey', 'AuthController@getPubKey');
+    });
 
-Route::group([
-    
-    'middleware' => 'api',
-    'namespace' => 'App\Http\Controllers',
-    'prefix' => 'auth'
-    
-], function ($router) {
-    
-    Route::post('register', 'AuthController@register');
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
-    Route::get('test', 'AuthController@test');
-    Route::post('card', 'AuthController@addCard');
-    Route::post('transaction', 'AuthController@addTransaction');
-    Route::get('pubkey', 'AuthController@getPubKey');
+    Route::post('card', 'CardController@addCard');
 
 });
