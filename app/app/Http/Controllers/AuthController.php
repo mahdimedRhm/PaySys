61c79cd29aacf36679da09281e6994e4d1d9ba84
RAHMOUN Mohamed Elmehdi <mahdimedrhm@gmail.com>
2021-01-13 22:27:10 +0100

    feat: decrypt data + add transaction

--- app/app/Http/Controllers/AuthController.php
+++ app/app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Card;
+use App\Models\Transaction;
 use App\Models\Rsakey;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
@@ -113,6 +114,7 @@ class AuthController extends Controller
         $keys = $this->generateRSAKeys();
         $rsapubkey = Rsakey::create(['key' => $keys['pub']]);
         $rsapubkey->user()->save($user);
+
         Storage::disk('local')->put('/rsa/'.$user->id . 'rsakey', $keys['prv']);
 
         $card = Card::create($this->generateCard());
@@ -131,6 +133,7 @@ class AuthController extends Controller
     }
 
     private function generateRSAKeys(){
+        $user = Auth::user() ?: null;
         $config = array(
             "digest_alg" => "sha512",
             "private_key_bits" => 2048,
@@ -152,5 +155,91 @@ class AuthController extends Controller
             "prv" => $privkey
         ];
 
+    }
+    public function getPubKey(){
+        $user = Auth::user() ?: null;
+        if (!$user){
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        if (!$user->rsakey){
+            return response()->json(['error' => 'No key'], 401);
+        }
+
+        return ($user->rsakey->key);
+    }
+
+    public function addTransaction(Request $request){
+        $user = Auth::user() ?: null;
+        $data = $this->decryptedData($request);
+        
+        // Check if auth user have card
+
+        // if ($user->card->code != $data['from']['code'] || $user->card->key != $data['from']['key']){
+        //     return response()->json(['error' => 'Its not your card'], 401);
+        // }
+
+        // if(!$this->checkIfcardExist($data['to'])){
+        //     return response()->json(['error' => 'Reciever card not found'], 401);
+        // }
+
+        // if($user->card->code == $data['to']['code'] && $user->card->key == $data['to']['key']){
+        //     return response()->json(['error' => 'Very smart ... -_-'], 401);
+        // }
+
+        // if($user->card->amount < $request['amount']){
+        //     return response()->json(['error' => "Your poor XD"], 401);
+        // }
+        $transaction = [
+            'from_id' => $this->getCardId($data['from']),
+            'to_id'   => $this->getCardId($data['to']),
+            // 'amount'  => (float)$request['amount']
+        ];
+
+        Transaction::create($transaction);
+
+        return $user->card;
+    }
+
+    private function checkIfcardExist($card){
+        return Card::where('key', $card['key'])
+            ->where('code', $card['code'])
+            ->get()
+            ->count() == 1;
+    }
+    
+    private function getCardId($card){
+        return Card::where('key', $card['key'])
+            ->where('code', $card['code'])
+            ->first()
+            ->id;
+    }
+
+    private function updateCard($card, $amount){
+        Card::where('key', $card['key'])
+            ->where('code', $card['code'])
+            ->first()
+            ->id;
+    }
+
+    private function decryptedData($data){
+        return [
+            "from" => [
+                "code" => $this->decryptRSA($data['from']['code']),
+                "key" => $this->decryptRSA($data['from']['key']),
+            ],
+            "to" => [
+                "code" => $this->decryptRSA($data['to']['code']),
+                "key" => $this->decryptRSA($data['to']['key']),
+            ]
+        ];
+    }
+
+    private function decryptRSA($data){
+        $user = Auth::user() ?: null;
+        $prvKey = Storage::disk('local')->get('/rsa/'. $user->id . 'rsakey');
+        $data = base64_decode($data);
+        openssl_private_decrypt ( $data , $decrypted , $prvKey);
+        return $decrypted;
     }
 }
